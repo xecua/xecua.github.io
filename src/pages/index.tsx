2ca4ca7c2a2c11a708a1696add5aadd2de7f8367
xecua <courser4848@gmail.com>
2020-06-10 01:46:38 +0900

    icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import BookIcon from '@material-ui/icons/Book';
 
 const Index: React.FC = () => {
   return (
     <>
-      <a href="https://xecua.hatenablog.com/">ブログ</a>
+      <a href="https://xecua.hatenablog.com/">
+        <BookIcon /> ブログ
+      </a>
       <hr />
       改修中…
     </>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
