6426f113eb4c3a917d17fea9deabcb1b2a3cab40
xecua <courser4848@gmail.com>
2020-06-12 01:13:02 +0900

    prettierと和解

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,31 +3,31 @@ import Header from '@/components/Header';
 import ThemeProvider from '@/components/ThemeProvider';
 import { Typography, Box, Link, makeStyles } from '@material-ui/core';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   box: {
-    marginTop: theme.spacing(2)
+    marginTop: theme.spacing(2),
   },
   string: {
-    marginLeft: theme.spacing(2)
-  }
+    marginLeft: theme.spacing(2),
+  },
 }));
 
 const Index: React.FC = () => {
   const classes = useStyles();
-  
+
   return (
     <ThemeProvider>
       <Header />
       <Box className={classes.box} component="div">
         <Typography className={classes.string}>
-          <Link href="https://xecua.hatenablog.com/" color="inherit">ブログ</Link>
+          <Link href="https://xecua.hatenablog.com/" color="inherit">
+            ブログ
+          </Link>
         </Typography>
       </Box>
       <hr />
       <Box className={classes.box}>
-        <Typography className={classes.string}>
-          改修中..
-        </Typography>
+        <Typography className={classes.string}>改修中..</Typography>
       </Box>
     </ThemeProvider>
   );
