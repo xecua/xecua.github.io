10aa27f8dcd49bd4c8d013e719c8918394465300
xecua <courser4848@gmail.com>
2020-06-12 15:05:05 +0900

    n

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,29 @@
 import React from 'react';
-import Header from '@/components/Header';
-import ThemeProvider from '@/components/ThemeProvider';
 import { Typography, Box, Link, makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   box: {
     marginTop: theme.spacing(2),
-  },
-  string: {
     marginLeft: theme.spacing(2),
   },
 }));
 
 const Index: React.FC = () => {
   const classes = useStyles();
-
   return (
-    <ThemeProvider>
-      <Header />
+    <>
       <Box className={classes.box} component="div">
-        <Typography className={classes.string}>
-          <Link href="https://xecua.hatenablog.com/" color="inherit">
+        <Typography>
+          →<Link href="https://xecua.hatenablog.com/">
             ブログ
-          </Link>
+          </Link>←
         </Typography>
       </Box>
       <hr />
       <Box className={classes.box}>
-        <Typography className={classes.string}>改修中..</Typography>
+        <Typography>改修中..</Typography>
       </Box>
-    </ThemeProvider>
+    </>
   );
 };
 
