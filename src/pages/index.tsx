3a6bb914e97b467c7661360a2523b9413317df28
xecua <courser4848@gmail.com>
2020-07-04 03:16:08 +0900

    ちょっと整理

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Typography, Box, Link, makeStyles } from '@material-ui/core';
+import { Typography, Link, makeStyles, Button, Grid } from '@material-ui/core';
+import { GitHub, Twitter } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
   box: {
     marginTop: theme.spacing(2),
     marginLeft: theme.spacing(2),
+  },
+  textTransformNone: {
+    textTransform: 'none',
   },
 }));
 
@@ -12,19 +16,38 @@ const Index: React.FC = () => {
   const classes = useStyles();
   return (
     <>
-      <Box className={classes.box} component="div">
-        <Typography>
-          →
-          <Link href="https://xecua.hatenablog.com/" color="inherit">
-            ブログ
-          </Link>
-          ←
-        </Typography>
-      </Box>
-      <hr />
-      <Box className={classes.box}>
-        <Typography>改修中..</Typography>
-      </Box>
+      <Grid container spacing={3} className={classes.box}>
+        <Grid item>
+          <Typography variant="h4" component="h2">
+            xecua
+          </Typography>
+        </Grid>
+        <Grid container spacing={1}>
+          <Grid item>
+            <Button
+              className={classes.textTransformNone}
+              variant="contained"
+              href="https://github.com/xecua"
+              startIcon={<GitHub />}>
+              GitHub: xecua
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button
+              className={classes.textTransformNone}
+              variant="contained"
+              href="https://twitter.com/xecual"
+              startIcon={<Twitter />}>
+              Twitter: @xecual
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button href="https://xecua.hatenablog.com/" variant="outlined">
+              ブログ
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
     </>
   );
 };
