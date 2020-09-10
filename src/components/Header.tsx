26b2295441833e169343ebd4aa1ba1523887ffa6
xecua <courser4848@gmail.com>
2020-09-10 19:15:42 +0900

    po

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,6 +14,8 @@ import {
   SvgIconTypeMap,
   ListItemIcon,
   ListItemText,
+  // SvgIcon,
+  // SvgIconProps,
 } from '@material-ui/core';
 import {
   GitHub,
@@ -21,6 +23,7 @@ import {
   Menu as MenuIcon,
   Person,
   Folder,
+  Edit,
 } from '@material-ui/icons';
 import { useRouter } from 'next/router';
 import NextLink from 'next/link';
@@ -53,6 +56,7 @@ const useStyles = makeStyles((theme) =>
       display: 'inline-block',
       marginRight: theme.spacing(2),
       boxSizing: 'content-box',
+      cursor: 'default',
     },
     menuItem: {
       boxSizing: 'content-box',
@@ -88,6 +92,15 @@ type Menu<T extends OverridableTypeMap> = {
   label: string;
 };
 
+// const BlogIcon = (props: SvgIconProps) => (
+//   <SvgIcon {...props} viewBox="0 0 300 300">
+//     <path
+//       fill="#333"
+//       d="M149.999 248.909c-54.537 0-98.906-44.367-98.906-98.909 0-54.537 44.369-98.909 98.906-98.909 54.545 0 98.908 44.372 98.908 98.909 0 54.542-44.363 98.909-98.908 98.909zm0-185.238c-47.601 0-86.33 38.723-86.33 86.329 0 47.605 38.729 86.332 86.33 86.332 47.61 0 86.338-38.727 86.338-86.332 0-47.606-38.728-86.329-86.338-86.329zM161.52 101.16c-4.832-9.785-7.783-19.3-9.273-24.845v70.055c2.447.917 4.197 3.257 4.197 6.021 0 3.559-2.887 6.442-6.443 6.442-3.56 0-6.443-2.885-6.443-6.442 0-2.896 1.925-5.317 4.558-6.131v-70.019c-1.485 5.531-4.438 15.092-9.293 24.919-7.571 15.314-17.009 28.823-17.009 28.823l6.036 82.598s5.736 6.401 22.31 6.41h.023c16.573-.009 22.312-6.41 22.312-6.41l6.035-82.598c-.003 0-9.441-13.508-17.01-28.823z"
+//     />
+//   </SvgIcon>
+// );
+
 const Header: React.FC = () => {
   const classes = useStyles();
   const router = useRouter();
@@ -118,6 +131,7 @@ const Header: React.FC = () => {
               <NextLink href={item.link} key={i} passHref>
                 <span className={classes.menuItemWrapper}>
                   <Typography
+                    variant="button"
                     component="div"
                     className={clsx(
                       classes.menuItem,
@@ -133,11 +147,14 @@ const Header: React.FC = () => {
           </Hidden>
 
           <div className={classes.spacer} />
-          <IconButton href="https://github.com/xecua">
+          <IconButton title="GitHub" href="https://github.com/xecua">
             <GitHub />
           </IconButton>
-          <IconButton href="https://twitter.com/xecual">
+          <IconButton title="Twitter" href="https://twitter.com/xecual">
             <Twitter />
+          </IconButton>
+          <IconButton title="Blog" href="https://xecua.hatenablog.com/">
+            <Edit />
           </IconButton>
         </Toolbar>
       </AppBar>
