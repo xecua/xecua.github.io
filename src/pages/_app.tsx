0f9e1034783479424575977372a9befbc806d679
xecua <courser4848@gmail.com>
2020-09-10 14:55:49 +0900

    ssr

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,14 @@ import Header from '@/components/Header';
 
 // eslint-disable-next-line react/prop-types
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <ThemeProvider>
       <Header />
