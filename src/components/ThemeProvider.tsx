a92450a99d9ce4529c5a4c45de495e5a90cb97dc
xecua <courser4848@gmail.com>
2022-01-31 19:37:45 +0900

    デフォルト値というだけにした

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -10,20 +10,24 @@ import CssBaseline from '@mui/material/CssBaseline';
 export const PrefersDarkModeContext = React.createContext(null);
 
 export const CustomThemeProvider: React.FC = (props) => {
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-  const [prefersDarkModeManual, setPrefersDarkModeManual] =
-    React.useState(false);
+  const prefersDarkModeOriginal = useMediaQuery(
+    '(prefers-color-scheme: dark)',
+    { noSsr: true }
+  );
+  const [prefersDarkMode, setPrefersDarkMode] = React.useState(
+    prefersDarkModeOriginal
+  );
 
   const theme = React.useMemo(
     () =>
       createTheme({
         palette: {
-          mode: prefersDarkMode || prefersDarkModeManual ? 'dark' : 'light',
+          mode: prefersDarkMode ? 'dark' : 'light',
           primary: { main: prefersDarkMode ? '#52A6FB' : '#0067D3' },
           secondary: { main: prefersDarkMode ? '#a1887f' : '#5d4037' },
         },
       }),
-    [prefersDarkMode, prefersDarkModeManual]
+    [prefersDarkMode]
   );
 
   return (
@@ -32,8 +36,8 @@ export const CustomThemeProvider: React.FC = (props) => {
         <CssBaseline />
         <PrefersDarkModeContext.Provider
           value={{
-            prefersDarkMode: prefersDarkModeManual,
-            setPrefersDarkMode: setPrefersDarkModeManual,
+            prefersDarkMode,
+            setPrefersDarkMode,
           }}>
           {props.children}
         </PrefersDarkModeContext.Provider>
