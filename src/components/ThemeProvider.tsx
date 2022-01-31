fb875c2d0125044f3b750766bdb2263e6ee3eb47
xecua <courser4848@gmail.com>
2022-01-31 19:26:31 +0900

    switch

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -3,36 +3,40 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 import {
   createTheme,
   ThemeProvider,
-  Theme,
   StyledEngineProvider,
 } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
-declare module '@mui/styles/defaultTheme' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface DefaultTheme extends Theme {}
-}
+export const PrefersDarkModeContext = React.createContext(null);
 
 export const CustomThemeProvider: React.FC = (props) => {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const [prefersDarkModeManual, setPrefersDarkModeManual] =
+    React.useState(false);
 
   const theme = React.useMemo(
     () =>
       createTheme({
         palette: {
-          mode: prefersDarkMode ? 'dark' : 'light',
+          mode: prefersDarkMode || prefersDarkModeManual ? 'dark' : 'light',
           primary: { main: prefersDarkMode ? '#52A6FB' : '#0067D3' },
           secondary: { main: prefersDarkMode ? '#a1887f' : '#5d4037' },
         },
       }),
-    [prefersDarkMode]
+    [prefersDarkMode, prefersDarkModeManual]
   );
 
   return (
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        {props.children}
+        <PrefersDarkModeContext.Provider
+          value={{
+            prefersDarkMode: prefersDarkModeManual,
+            setPrefersDarkMode: setPrefersDarkModeManual,
+          }}>
+          {props.children}
+        </PrefersDarkModeContext.Provider>
       </ThemeProvider>
     </StyledEngineProvider>
   );
