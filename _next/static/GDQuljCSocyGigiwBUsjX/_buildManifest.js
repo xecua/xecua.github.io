1fae5be970fd51db449b58cf9ab582cf346410d0
xecua <xecua@users.noreply.github.com>
2022-06-03 18:02:17 +0000

    deploy: 0a88a1863c6eb87fb2cec29a3e46655e62aaf64c

--- _next/static/GDQuljCSocyGigiwBUsjX/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-712e930658ca5e11.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/works":["static/chunks/pages/works-e607d51ff874f547.js"],sortedPages:["/","/_app","/_error","/works"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
