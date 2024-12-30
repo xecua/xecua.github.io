718aeb4032677453e12c11144edb99426937cf5d
Shiba Shunta <contact@caffeine.page>
2024-12-30 19:40:33 +0900

    cheatsheet (wip)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,21 @@
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 
+import remarkLinkCard from 'remark-link-card';
+import rehypeRaw from 'rehype-raw';
+import rehypeExternalLinks from 'rehype-external-links';
+
 export default defineConfig({
   integrations: [icon()],
   server: { port: 3000 },
+  markdown: {
+    remarkPlugins: [[remarkLinkCard, { shortenUrl: true }]],
+    rehypePlugins: [
+      rehypeRaw,
+      [
+        rehypeExternalLinks,
+        { target: '_blank', rel: ['noopener', 'noreferrer'] },
+      ],
+    ],
+  },
 });
